3ad6de95d28dcd1d03ab4b1e82b821452e100fb6
FranziskaScheller <60098877+FranziskaScheller@users.noreply.github.com>
2022-10-24 12:59:44 +0200

    Save current changes

--- DFClassifier.py
+++ DFClassifier.py
@@ -311,64 +311,120 @@ from sklearn.metrics import roc_curve
 
 brf200 = BalancedRandomForestClassifier(n_estimators=200, random_state=0).fit(X_train, y_train)
 pred_brf200 = brf200.predict_proba(X_test)
-
+#
 brf400 = BalancedRandomForestClassifier(n_estimators=400, random_state=0).fit(X_train, y_train)
 pred_brf400 = brf400.predict_proba(X_test)
+#
+# rfc200 = RandomForestClassifier(random_state=0, n_estimators= 200).fit(X_train, y_train)
+# pred_rfc200 = rfc200.predict_proba(X_test)
+#
+# rfc400 = RandomForestClassifier(random_state=0, n_estimators= 400).fit(X_train, y_train)
+# pred_rfc400 = rfc400.predict_proba(X_test)
+#
+# # calculate roc curves
+# red_names = [pred_brf200, pred_brf400, pred_rfc200, pred_rfc400]
+# red_labels = ['BRF 200 Trees', 'BRF 400 Trees', 'RF 200 Trees', 'RF 400 Trees']
+# fig, ax = plt.subplots(2, 2, figsize=(16, 16), dpi=120)
+# k = 0
+# for i in range(0, 4):
+#     fpr, tpr, thresholds = roc_curve(y_test, red_names[i][:, 1])
+#     # calculate the g-mean for each threshold
+#     gmeans = sqrt(tpr * (1 - fpr))
+#     # locate the index of the largest g-mean
+#     ix = argmax(gmeans)
+#     print('Best Threshold=%f, G-Mean=%.3f' % (thresholds[ix], gmeans[ix]))
+#     weighted_acc = 0.8 * tpr + 0.2 * (1 - fpr)
+#     # locate the index of the largest g-mean
+#     ix_weighted_acc = argmax(weighted_acc)
+#     print('Best Threshold=%f, weighted_acc=%.3f' % (thresholds[ix_weighted_acc], weighted_acc[ix_weighted_acc]))
+#     print('AUC: ' + str( roc_auc_score(y_test, red_names[i][:, 1])))
+#
+#     # plot the roc curve for the model
+#     plt.subplot(2, 2, (i + 1))
+#     plt.title(str(red_labels[k]), fontsize=24)
+#     plt.plot([0, 1], [0, 1], linestyle='--', label='No Skill', color='mediumaquamarine')
+#     plt.plot(fpr, tpr, marker='.', label='ROC curve', color='teal')
+#     plt.scatter(fpr[ix], tpr[ix], marker='o', s=80, color='indigo',
+#                 label='Best G-Mean ' + str(np.round(max(gmeans), 3)))
+#     plt.scatter(fpr[ix_weighted_acc], tpr[ix_weighted_acc], marker='o', s=80, color='mediumorchid',
+#                 label='Best Weighted Accuracy ' + str(np.round(max(weighted_acc), 3)))
+#     # axis labels
+#     plt.xlabel('1 - True Negative Rate', fontsize=20)
+#     plt.ylabel('True Positive Rate', fontsize=20)
+#     plt.xticks(fontsize=19)
+#     plt.yticks(fontsize=19)
+#     plt.legend(fontsize=18)
+#     k = k + 1
+#     plt.tight_layout()
+#     plt.savefig(
+#         'ROCCurves.png')
+#     # show the plot
+#
+# pyplot.show()
+#
+# red_names = [[pred_brf200, pred_brf400], [pred_rfc200, pred_rfc400]]
+# red_labels = ['Balanced Random Forest with 200 and 400 Trees', 'Random Forest with 200 and 400 Trees']
+# fig, ax = plt.subplots(2, 1, figsize=(12, 26), dpi=120)
+# k = 0
+# for i in range(0, 2):
+#     fpr_200, tpr_200, thresholds_200 = roc_curve(y_test, red_names[i][0][:, 1])
+#     fpr_400, tpr_400, thresholds_400 = roc_curve(y_test, red_names[i][1][:, 1])
+#     # calculate the g-mean for each threshold
+#     gmeans_200 = sqrt(tpr_200 * (1 - fpr_200))
+#     gmeans_400 = sqrt(tpr_400 * (1 - fpr_400))
+#     # locate the index of the largest g-mean
+#     ix_200 = argmax(gmeans_200)
+#     ix_400 = argmax(gmeans_400)
+#     print('Best Threshold200=%f, G-Mean400=%.3f' % (thresholds_200[ix_200], gmeans_200[ix_200]))
+#     print('Best Threshold400=%f, G-Mean400=%.3f' % (thresholds_400[ix_400], gmeans_400[ix_400]))
+#     weighted_acc_200 = 0.666 * tpr_200 + (1-0.666) * (1 - fpr_200)
+#     # locate the index of the largest g-mean
+#     ix_weighted_acc_200 = argmax(weighted_acc_200)
+#     print('Best Threshold200=%f, weighted_acc200=%.3f' % (thresholds_200[ix_weighted_acc_200], weighted_acc_200[ix_weighted_acc_200]))
+#     print('AUC200: ' + str( roc_auc_score(y_test, red_names[i][0][:, 1])))
+#     weighted_acc_400 = 0.666 * tpr_400 + (1-0.666) * (1 - fpr_400)
+#     # locate the index of the largest g-mean
+#     ix_weighted_acc_400 = argmax(weighted_acc_400)
+#     print('Best Threshold_400=%f, weighted_acc_400=%.3f' % (thresholds_400[ix_weighted_acc_400], weighted_acc_400[ix_weighted_acc_400]))
+#     print('AUC_400: ' + str( roc_auc_score(y_test, red_names[i][1][:, 1])))
+#     # plot the roc curve for the model
+#     plt.subplot(2, 1, (i + 1))
+#     plt.title(str(red_labels[k]), fontsize=30)
+#     plt.plot([0, 1], [0, 1], linestyle='--', label='No Skill', color='mediumaquamarine', linewidth=3.5, zorder=-1)
+#     plt.plot(fpr_200, tpr_200, marker='.', label='ROC curve with 200 Trees', color='teal', linewidth=3.5, zorder=-1)
+#     plt.plot(fpr_400, tpr_400, marker='.', label='ROC curve with 400 Trees', color='darkslategrey', linewidth=3.5, zorder=-1)
+#     plt.scatter(fpr_200[ix_200], tpr_200[ix_200], marker='o', s=160, color='indigo',
+#                 label='Best G-Mean with 200 Trees ' + str(np.round(max(gmeans_200), 3)), zorder=1)
+#     plt.scatter(fpr_200[ix_weighted_acc_200], tpr_200[ix_weighted_acc_200], marker='o', s=160, color='mediumorchid',
+#                 label='Best Weighted Accuracy with 200 Trees ' + str(np.round(max(weighted_acc_200), 3)), zorder=1)
+#     plt.scatter(fpr_400[ix_400], tpr_400[ix_400], marker='o', s=160, color='gold',
+#                 label='Best G-Mean with 400 Trees ' + str(np.round(max(gmeans_400), 3)), zorder=1)
+#     plt.scatter(fpr_400[ix_weighted_acc_400], tpr_400[ix_weighted_acc_400], marker='o', s=160, color='orange',
+#                 label='Best Weighted Accuracy with 400 Trees ' + str(np.round(max(weighted_acc_400), 3)), zorder=1)
+#     # axis labels
+#     plt.xlabel('1 - True Negative Rate', fontsize=27)
+#     plt.ylabel('True Positive Rate', fontsize=27)
+#     plt.xticks(fontsize=25)
+#     plt.yticks(fontsize=25)
+#     plt.legend(fontsize=22)
+#     k = k + 1
+#     plt.tight_layout()
+#     #plt.savefig(
+#     #    'ROCCurves.png')
+#     # show the plot
+# pyplot.show()
+#
 
-rfc200 = RandomForestClassifier(random_state=0, n_estimators= 200).fit(X_train, y_train)
-pred_rfc200 = rfc200.predict_proba(X_test)
-
-rfc400 = RandomForestClassifier(random_state=0, n_estimators= 400).fit(X_train, y_train)
-pred_rfc400 = rfc400.predict_proba(X_test)
-
-# calculate roc curves
-red_names = [pred_brf200, pred_brf400, pred_rfc200, pred_rfc400]
-red_labels = ['BRF 200 Trees', 'BRF 400 Trees', 'RF 200 Trees', 'RF 400 Trees']
-fig, ax = plt.subplots(2, 2, figsize=(16, 16), dpi=120)
-k = 0
-for i in range(0, 4):
-    fpr, tpr, thresholds = roc_curve(y_test, red_names[i][:, 1])
-    # calculate the g-mean for each threshold
-    gmeans = sqrt(tpr * (1 - fpr))
-    # locate the index of the largest g-mean
-    ix = argmax(gmeans)
-    print('Best Threshold=%f, G-Mean=%.3f' % (thresholds[ix], gmeans[ix]))
-    weighted_acc = 0.8 * tpr + 0.2 * (1 - fpr)
-    # locate the index of the largest g-mean
-    ix_weighted_acc = argmax(weighted_acc)
-    print('Best Threshold=%f, weighted_acc=%.3f' % (thresholds[ix], weighted_acc[ix]))
-    # plot the roc curve for the model
-    plt.subplot(2, 2, (i + 1))
-    plt.title(str(red_labels[k]), fontsize=24)
-    plt.plot([0, 1], [0, 1], linestyle='--', label='No Skill', color='mediumaquamarine')
-    plt.plot(fpr, tpr, marker='.', label='ROC curve', color='teal')
-    plt.scatter(fpr[ix], tpr[ix], marker='o', s=80, color='indigo',
-                label='Best G-Mean ' + str(np.round(max(gmeans), 3)))
-    plt.scatter(fpr[ix_weighted_acc], tpr[ix_weighted_acc], marker='o', s=80, color='mediumorchid',
-                label='Best Weighted Accuracy ' + str(np.round(max(weighted_acc), 3)))
-    # axis labels
-    plt.xlabel('False Positive Rate', fontsize=20)
-    plt.ylabel('True Positive Rate', fontsize=20)
-    plt.xticks(fontsize=19)
-    plt.yticks(fontsize=19)
-    plt.legend(fontsize=18)
-    k = k + 1
-    plt.tight_layout()
-    plt.savefig(
-        'ROCCurves.png')
-    # show the plot
-
-pyplot.show()
 
 
-rfc = RandomForestClassifier(random_state=0, min_samples_leaf = 2, n_estimators= 200).fit(X_train, y_train)
+rfc = RandomForestClassifier(random_state=0, n_estimators= 400).fit(X_train, y_train)
 pred_rfc = rfc.predict_proba(X_test)
 #
 clf_pred_m_rfc = pd.DataFrame(pred_rfc)
 clf_pred_m_rfc['real'] = y_test.values
 #
 clf_res_y_rfc = np.zeros(len(y_test))
-clf_res_y_rfc[pred_rfc[:,1] >= 0.006] = 1
+clf_res_y_rfc[pred_rfc[:,1] >= 0.0025] = 1
 clf_res_y_rfc = pd.DataFrame(clf_res_y_rfc)
 #
 conf_mat_logreg_rfc = confusion_matrix(y_test, clf_res_y_rfc.values)
@@ -376,28 +432,28 @@ precision_logreg_rfc = precision_score(y_test, clf_res_y_rfc.values)
 recall_logreg_rfc = recall_score(y_test, clf_res_y_rfc.values)
 
 #brf = BalancedRandomForestClassifier(n_estimators=50, random_state=0)
-brf = BalancedRandomForestClassifier(n_estimators=200, random_state=0).fit(X_train, y_train)
+brf = BalancedRandomForestClassifier(n_estimators=400, random_state=0).fit(X_train, y_train)
 pred_brf = brf.predict_proba(X_test)
 brf_feature_imp = brf.feature_importances_
 forest_importances = pd.Series(brf_feature_imp, index=X.columns)
 forest_importances = forest_importances.sort_values()
 
-std = np.std([tree.feature_importances_ for tree in brf.estimators_], axis=0)
-fig, ax = plt.subplots(figsize=(16, 25), dpi=100)
-forest_importances.plot.barh(ax=ax, color = 'teal')
-ax.set_title("Feature importances",  fontsize=33)
-plt.rc('xtick', labelsize=23)
-plt.rc('ytick', labelsize=23)
-fig.tight_layout()
-plt.savefig(
-   'Feature_importance_all.png')
-fig.show()
+# std = np.std([tree.feature_importances_ for tree in brf.estimators_], axis=0)
+# fig, ax = plt.subplots(figsize=(16, 25), dpi=100)
+# forest_importances.plot.barh(ax=ax, color = 'teal')
+# ax.set_title("Feature importances",  fontsize=33)
+# plt.rc('xtick', labelsize=23)
+# plt.rc('ytick', labelsize=23)
+# fig.tight_layout()
+# plt.savefig(
+#    'Feature_importance_all.png')
+# fig.show()
 #
 clf_pred_m_brf = pd.DataFrame(pred_brf)
 clf_pred_m_brf['real'] = y_test.values
 #
 clf_res_y_brf = np.zeros(len(y_test))
-clf_res_y_brf[pred_brf[:,1] >= 0.006] = 1
+clf_res_y_brf[pred_brf[:,1] >= 0.198] = 1
 clf_res_y_brf = pd.DataFrame(clf_res_y_brf)
 #
 conf_mat_logreg_brf = confusion_matrix(y_test, clf_res_y_brf.values)
@@ -407,98 +463,101 @@ target_names = ['NoDF', 'DF']  # doctest : +NORMALIZE_WHITESPACE
 res_brt = classification_report_imbalanced(y_test.values, clf_res_y_brf.values, target_names=target_names)
 res_rf = classification_report_imbalanced(y_test.values, clf_res_y_rfc.values, target_names=target_names)
 
-r_auc_sc = roc_auc_score(y_test, pred_brf[:, 1])
-from numpy import sqrt
-from numpy import argmax
-
-from sklearn.linear_model import LogisticRegression
-from matplotlib import pyplot
-from sklearn.metrics import roc_curve
-# calculate roc curves
-fpr, tpr, thresholds = roc_curve(y_test, pred_brf[:, 1])
-# calculate the g-mean for each threshold
-gmeans = sqrt(tpr * (1-fpr))
-# locate the index of the largest g-mean
-ix = argmax(gmeans)
-print('Best Threshold=%f, G-Mean=%.3f' % (thresholds[ix], gmeans[ix]))
-weighted_acc = 0.8*tpr + 0.2*(1-fpr)
-# locate the index of the largest g-mean
-ix_weighted_acc = argmax(weighted_acc)
-print('Best Threshold=%f, weighted_acc=%.3f' % (thresholds[ix], weighted_acc[ix]))
-# plot the roc curve for the model
-pyplot.plot([0,1], [0,1], linestyle='--', label='No Skill')
-pyplot.plot(fpr, tpr, marker='.', label='Logistic')
-pyplot.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Best_G-Mean')
-pyplot.scatter(fpr[ix_weighted_acc], tpr[ix_weighted_acc], marker='o', color='red', label='Best_WeightedAcc')
-# axis labels
-pyplot.xlabel('False Positive Rate')
-pyplot.ylabel('True Positive Rate')
-pyplot.legend()
-# show the plot
-pyplot.show()
-
-weighted_acc = 0.8*tpr + 0.2*(1-fpr)
-# locate the index of the largest g-mean
-ix_weighted_acc = argmax(weighted_acc)
-print('Best Threshold=%f, weighted_acc=%.3f' % (thresholds[ix], weighted_acc[ix]))
-# plot the roc curve for the model
-pyplot.plot([0,1], [0,1], linestyle='--', label='No Skill')
-pyplot.plot(fpr, tpr, marker='.', label='Logistic')
-pyplot.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Best')
-# axis labels
-pyplot.xlabel('False Positive Rate')
-pyplot.ylabel('True Positive Rate')
-pyplot.legend()
-# show the plot
-pyplot.show()
-
-
-# ---- Evaluation ----
 dummy_clf = DummyClassifier(strategy="stratified").fit(X_train, y_train)
 pred_dummy_clf = dummy_clf.predict_proba(X_test)
 
-dummy_clf = DummyClassifier(strategy="stratified")
-scoring = ["accuracy", "balanced_accuracy"]
-cv_result = cross_validate(dummy_clf, df_res, y_test.values, scoring=scoring)
-index = []
-scores = {"Accuracy": [], "Balanced accuracy": []}
-index += ["Dummy classifier"]
-cv_result = cross_validate(dummy_clf, df_res, y_test.values, scoring=scoring)
-scores["Accuracy"].append(cv_result["test_accuracy"].mean())
-scores["Balanced accuracy"].append(cv_result["test_balanced_accuracy"].mean())
-
-df_scores = pd.DataFrame(scores, index=index)
-#df_scores
-
-clf = BaggingClassifier(base_estimator=SVC(),n_estimators=150, random_state=0).fit(X_train, y_train)
-
-#rfc = RandomForestClassifier(random_state=0, min_samples_leaf = 2, n_estimators= 200).fit(X_train, y_train)
-pred_clf = clf.predict_proba(X_test)
+# r_auc_sc = roc_auc_score(y_test, pred_brf[:, 1])
+# from numpy import sqrt
+# from numpy import argmax
 #
-clf_pred_m_clf = pd.DataFrame(pred_clf)
-clf_pred_m_clf['real'] = y_test.values
+# from sklearn.linear_model import LogisticRegression
+# from matplotlib import pyplot
+# from sklearn.metrics import roc_curve
+# # calculate roc curves
+# fpr, tpr, thresholds = roc_curve(y_test, pred_brf[:, 1])
+# # calculate the g-mean for each threshold
+# gmeans = sqrt(tpr * (1-fpr))
+# # locate the index of the largest g-mean
+# ix = argmax(gmeans)
+# print('Best Threshold=%f, G-Mean=%.3f' % (thresholds[ix], gmeans[ix]))
+# weighted_acc = 0.8*tpr + 0.2*(1-fpr)
+# # locate the index of the largest g-mean
+# ix_weighted_acc = argmax(weighted_acc)
+# print('Best Threshold=%f, weighted_acc=%.3f' % (thresholds[ix], weighted_acc[ix]))
+# # plot the roc curve for the model
+# pyplot.plot([0,1], [0,1], linestyle='--', label='No Skill')
+# pyplot.plot(fpr, tpr, marker='.', label='Logistic')
+# pyplot.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Best_G-Mean')
+# pyplot.scatter(fpr[ix_weighted_acc], tpr[ix_weighted_acc], marker='o', color='red', label='Best_WeightedAcc')
+# # axis labels
+# pyplot.xlabel('False Positive Rate')
+# pyplot.ylabel('True Positive Rate')
+# pyplot.legend()
+# # show the plot
+# pyplot.show()
 #
-clf_res_y_clf = np.zeros(len(y_test))
-clf_res_y_clf[pred_clf[:,1] >= 0.006] = 1
-clf_res_y_clf = pd.DataFrame(clf_res_y_clf)
+# weighted_acc = 0.8*tpr + 0.2*(1-fpr)
+# # locate the index of the largest g-mean
+# ix_weighted_acc = argmax(weighted_acc)
+# print('Best Threshold=%f, weighted_acc=%.3f' % (thresholds[ix], weighted_acc[ix]))
+# # plot the roc curve for the model
+# pyplot.plot([0,1], [0,1], linestyle='--', label='No Skill')
+# pyplot.plot(fpr, tpr, marker='.', label='Logistic')
+# pyplot.scatter(fpr[ix], tpr[ix], marker='o', color='black', label='Best')
+# # axis labels
+# pyplot.xlabel('False Positive Rate')
+# pyplot.ylabel('True Positive Rate')
+# pyplot.legend()
+# # show the plot
+# pyplot.show()
 #
-conf_mat_logreg_clf = confusion_matrix(y_test, clf_res_y_clf.values)
-precision_logreg_clf = precision_score(y_test, clf_res_y_clf.values)
-recall_logreg_clf = recall_score(y_test, clf_res_y_clf.values)
-
-
-clf = LogisticRegression(random_state=0, max_iter=600).fit(X_train, y_train)
-pred_cfl = clf.predict_proba(X_test)
-clf_pred_m = pd.DataFrame(pred_cfl)
-clf_pred_m['real'] = y_test.values
-
-clf_res_y = np.zeros(len(y_test))
-clf_res_y[pred_cfl[:,1] >= 0.006] = 1
-clf_res_y = pd.DataFrame(clf_res_y)
-
-conf_mat_logreg = confusion_matrix(y_test, clf_res_y.values)
-precision_logreg = precision_score(y_test, clf_res_y.values)
-recall_logreg = recall_score(y_test, clf_res_y.values)
+#
+# # ---- Evaluation ----
+# dummy_clf = DummyClassifier(strategy="stratified").fit(X_train, y_train)
+# pred_dummy_clf = dummy_clf.predict_proba(X_test)
+#
+# dummy_clf = DummyClassifier(strategy="stratified")
+# scoring = ["accuracy", "balanced_accuracy"]
+# cv_result = cross_validate(dummy_clf, df_res, y_test.values, scoring=scoring)
+# index = []
+# scores = {"Accuracy": [], "Balanced accuracy": []}
+# index += ["Dummy classifier"]
+# cv_result = cross_validate(dummy_clf, df_res, y_test.values, scoring=scoring)
+# scores["Accuracy"].append(cv_result["test_accuracy"].mean())
+# scores["Balanced accuracy"].append(cv_result["test_balanced_accuracy"].mean())
+#
+# df_scores = pd.DataFrame(scores, index=index)
+# #df_scores
+#
+# clf = BaggingClassifier(base_estimator=SVC(),n_estimators=150, random_state=0).fit(X_train, y_train)
+#
+# #rfc = RandomForestClassifier(random_state=0, min_samples_leaf = 2, n_estimators= 200).fit(X_train, y_train)
+# pred_clf = clf.predict_proba(X_test)
+# #
+# clf_pred_m_clf = pd.DataFrame(pred_clf)
+# clf_pred_m_clf['real'] = y_test.values
+# #
+# clf_res_y_clf = np.zeros(len(y_test))
+# clf_res_y_clf[pred_clf[:,1] >= 0.006] = 1
+# clf_res_y_clf = pd.DataFrame(clf_res_y_clf)
+# #
+# conf_mat_logreg_clf = confusion_matrix(y_test, clf_res_y_clf.values)
+# precision_logreg_clf = precision_score(y_test, clf_res_y_clf.values)
+# recall_logreg_clf = recall_score(y_test, clf_res_y_clf.values)
+#
+#
+# clf = LogisticRegression(random_state=0, max_iter=600).fit(X_train, y_train)
+# pred_cfl = clf.predict_proba(X_test)
+# clf_pred_m = pd.DataFrame(pred_cfl)
+# clf_pred_m['real'] = y_test.values
+#
+# clf_res_y = np.zeros(len(y_test))
+# clf_res_y[pred_cfl[:,1] >= 0.006] = 1
+# clf_res_y = pd.DataFrame(clf_res_y)
+#
+# conf_mat_logreg = confusion_matrix(y_test, clf_res_y.values)
+# precision_logreg = precision_score(y_test, clf_res_y.values)
+# recall_logreg = recall_score(y_test, clf_res_y.values)
 
 #---
 res_eval_df_probs = pd.DataFrame(mastertableDFclassifier[mastertableDFclassifier.index.isin(X_test.index)].Date, columns = ['Date'])
